5179a3e3b6d75fe3965ae004b087495a9942c300
omi-ttv <omarg2603@hotmail.com>
2024-02-12 08:24:15 -0600

    Conexión para evaluador
    Conexión hecha entre ventanas de evaluador

---
+++ Pages/EvaluatorPages/EvaluatorHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Posgrev_Frontend.Pages.EvaluatorPages
+{
+    public class EvaluatorHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
